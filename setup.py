5efaaaafc1d3254f7b8e2fd0b483e56e73b846a6
Egger, François <francois.egger@swatchgroup.com>
2017-03-06 16:27:22 +0100

    update author and gitignore

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 
 setup(name='netapp',
       version='0.1',
-      description='Netapp api',
-      author='Netapp',
+      description='Netapp zapi python modules',
+      author='François Egger',
       author_email='francois.egger@swatchgroup.com',
       url='https://www.netapp.com',
       packages=['netapp'],
